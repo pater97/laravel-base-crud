683a1dde2622e38a5a9597cc55870085886f5b5b
pater97 <giuseppepater997@gmail.com>
2022-01-19 15:41:03 +0100

    creata migration/seed e model: creata tabella,collegata e popolata

---
+++ database/seeds/ComicSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comic;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comics = config('comics_db.cards');
+
+        foreach($comics as $comic){
+            $_comic= new Comic();
+            $_comic->title=$comic['title'];
+            $_comic->description=$comic['description'];
+            $_comic->thumb=$comic['thumb'];
+            $_comic->price=$comic['price'];
+            $_comic->series=$comic['series'];
+            $_comic->sale_date=$comic['sale_date'];
+            $_comic->type=$comic['type'];
+            $_comic->save();
+        }
+    }
+}
