b272d696296333435ba20ede8e69940db974f9ca
pater97 <giuseppepater997@gmail.com>
2022-01-19 15:20:53 +0100

    impaginamento base(layout app con header e footer)

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,74 @@
+<footer>
+    <section class="footer_link position-relative">
+        <div class="container d-flex flex-wrap">
+            <div class="col-md-2">
+                <h2>
+                    DC COMICS
+                </h2>
+                <a href="">Characters</a>
+                <a href="">Comics</a>
+                <a href="">Movies</a>
+                <a href="">Tv</a>
+                <a href="">Games</a>
+                <a href="">Videos</a>
+                <a href="">News</a>
+                <H2>
+                    SHOP
+                </H2>
+                <a href="">Shop DC</a>
+                <a href="">Shop DC Collections</a>
+            </div>
+            <div class="col-md-2">
+                <h2>
+                    DC
+                </h2>
+                <a href="">Terms Of Use</a>
+                <a href="">Privacy Policy</a>
+                <a href="">Ad Choices</a>
+                <a href="">Advertaisign</a>
+                <a href="">Jobs</a>
+                <a href="">Subscription</a>
+                <a href="">Talent Workshop</a>
+                <a href="">CPSC Certificates</a>
+                <a href="">Ratings</a>
+                <a href="">Shop help</a>
+                <a href="">Contact Us</a>
+            </div>
+            <div class="col-md-2">
+                <h2>
+                    SITES
+                </h2>
+                <a href="">DC</a>
+                <a href="">MAD Magazine</a>
+                <a href="">DC Kids</a>
+                <a href="">DC Uninverse</a>
+                <a href="">DC Power Visa</a>
+            </div>
+            <div class="col-12">
+                <p>
+                    All site content TM and C 2020 DC ENTERTAINMENT,unless otherwise <span class="blue_link">noted here</span>. All rigths reserved.
+                    <span class="blue_link">Cookies Settings</span>
+                </p>
+            </div>
+            <div class="logo_footer position-absolute">
+            </div>
+        </div>
+    </section>
+    <section class="log_in container-fluid">
+        <div class="container">
+            <div class="sing_up">
+                <div>
+                    <a href="">SING-IP NOW!</a>
+                </div>
+            </div>
+            <div class="follow_us">
+                <a href="">FOLLOW US</a>
+                <img src="../../img/footer-facebook.png" alt="">
+                <img src="../../img/footer-twitter.png" alt="">
+                <img src="../../img/footer-youtube.png" alt="">
+                <img src="../../img/footer-pinterest.png" alt="">
+                <img src="../../img/footer-periscope.png" alt="">
+            </div>
+        </div>
+    </section>
+</footer>
